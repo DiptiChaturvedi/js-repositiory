26ba8a56af118528d81272201753554bb803d51e
DiptiChaturvedi <diptichaturvedi101@gmail.com>
2025-03-29 16:26:00 +0000

    container config done

---
+++ basic/DataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3
+    +5)      // code readability should be high
+
+// data types //
+
+let name= "dipti";
+let age = 23;
+let isLogged = false
+
+//  number ....2 power 53 
+//   bigint
+//   boolean
+//   string => ""
+//   null  => standlone value
+//   undefined
+//   symbol
---
+++ basic/VarConst.js
@@ -0,0 +1,11 @@
+const AccountID =1234;
+let password = "abhi";
+var name = "Abhishek Singh";
+email_id = "abhi@gmail.com"
+
+password = "node111";
+let AccountId =2222;
+console.log(password);
+console.table([password,name ,email_id, AccountId])
+
+
