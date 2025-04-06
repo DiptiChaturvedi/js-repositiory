c2aa6cc18e79d6ad6bba3c59f7f2a7907bcc030d
DiptiChaturvedi <diptichaturvedi101@gmail.com>
2025-04-06 03:31:02 +0000

    Conversion of datatypes

---
+++ basic/datatype_conversion.js
@@ -0,0 +1,35 @@
+let score = "33abs"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// check is actually converted into number or not//
+
+console.log(valueInNumber);
+
+// NaN = not a number //
+
+// "33" = 33
+// "33abs" = NaN 
+// true =      1
+
+
+let IsLoggedIn = "dipti"
+
+let boolenIsLoggedIn = Boolean(IsLoggedIn);
+console.log(boolenIsLoggedIn);
+
+// 1 conver into true
+// 0 -> false
+// string("dipti") => true
+// "" => false
+
+
+let number = 101
+
+let StringNumber = String(number);
+console.log(StringNumber);
+console.log(typeof StringNumber);
